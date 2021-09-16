ba4733b10275db21cc75489af69a7bf8f0a78d73
IvanSerov <VanSer1989@gmail.com>
2021-09-16 12:05:39 +0200

    first1

---
+++ src/sptv20_gitpro/Sptv20_GitPro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv20_gitpro;
+
+/**
+ *
+ * @author user
+ */
+public class Sptv20_GitPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("first");
+        // TODO code application logic here
+    }
+    
+}
